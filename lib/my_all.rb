ac9019bf60a30a7cfd0d5d9b980fdc031ee69247
BenDunjay <bendunjay@outlook.com>
2020-11-10 10:22:20 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,17 @@
-require 'pry'
+require "byebug"
 
 def my_all?(collection)
+  i = 0
+  block_return_values = []
+  while i < collection.length
+    block_return_values << yield(collection[i])
+    # byebug
+    i = i + 1
+  end
 
+  if block_return_values.include?(false)
+    false
+  else
+    true
+  end
 end
